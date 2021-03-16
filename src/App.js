1e17c6e1d830365978e60c76e38e3f0ec0df530b
anthony ervin <ajervin1@asu.edu>
2021-03-15 20:14:01 -0700

    css changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import './App.css'
 function App () {
 	return (
 		<div className="App">
-			<h2>Hello</h2>
+			<header>
+				<h2>Header</h2>
+			</header>
+			<section className="hero">
+				<h2>Hero</h2>
+			</section>
+			<section className="courses">
+				<h2>Courses</h2>
+			</section>
+			<section className="news">
+				<h2>News</h2>
+			</section>
+			<section className="testimonials">
+				<h2>Testimonials</h2>
+			</section>
+			<section className="footer">
+				<h2>Footer</h2>
+			</section>
 		</div>
 	)
 }
